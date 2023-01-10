719d574cd0829c0a93cfe2e5ae21db309ad47645
anandpothraj <anandpothraj11052001@gmail.com>
2023-01-10 20:57:22 +0530

    Created Footer

---
+++ frontend/src/components/Footer/SubFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SubFooter = () => {
+  return (
+    <div className='bg-dark text-light w-100 p-3'>
+        <div className="col-11 m-auto d-flex col-md-9 justify-content-between flex-column-reverse flex-md-row">
+            <p>© 2023 anandpothraj. All rights reserved.</p>
+            <p>Made with 💖 by Me!</p>
+            <p>v1.0.0</p>
+        </div>
+    </div>
+  )
+}
+
+export default SubFooter
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{minHeight:"80vh"}}>Home</div>
+  );
+};
+
+export default Home;
