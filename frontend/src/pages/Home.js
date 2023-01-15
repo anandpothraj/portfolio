d4cc7dfb2f42b965cdf2730c6983e3aa1fa198f4
anandpothraj <anandpothraj11052001@gmail.com>
2023-01-15 22:34:12 +0530

    Added Work Component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Profile from '../components/Home/Profile';
+import Work from "../components/Home/Work";
 
 const Home = () => {
   return (
     <div className="bg-dark">
       <Profile/>
+      <Work/>
     </div>
   );
 };
